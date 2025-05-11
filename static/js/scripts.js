1c22c97ab953ac9385a6bec202f0b92e87ab6521
Ignacio Vespali <163475042+nvoxer@users.noreply.github.com>
2025-05-11 16:59:54 -0300

    form encuesta y script votar

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,8 @@
     function confiar() {
       alert('No debiste haber confiado...');
     }
+
+    function votar() {
+      document.getElementById("formEncuesta").style.display = "none";
+      document.getElementById("mensajegracias").style.display = "block";
+    }
