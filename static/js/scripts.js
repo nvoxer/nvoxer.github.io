a072233bb75b6b72365e109c16c78b3f89f3fa06
Ignacio Vespali <163475042+nvoxer@users.noreply.github.com>
2025-05-11 20:30:11 -0300

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,8 @@
-    function confiar() {
-      alert('No debiste haber confiado...');
-    }
+function confiar() {
+  alert('No debiste haber confiado...');
+}
 
-    function votar() {
-      document.getElementById("formEncuesta").style.display = "none";
-      document.getElementById("mensajegracias").style.display = "block";
-    }
+function votar() {
+  document.getElementById("formEncuesta").style.display = "none";
+  document.getElementById("mensajegracias").style.display = "block";
+}
